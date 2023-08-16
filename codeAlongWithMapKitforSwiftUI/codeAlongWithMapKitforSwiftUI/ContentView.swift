6e6782890577ed065f3a27b07a4a1b9ecbb74dd3
Laurent Brusa <wrmultitudes@gmail.com>
2023-08-16 13:53:18 +0200

    start

---
+++ codeAlongWithMapKitforSwiftUI/codeAlongWithMapKitforSwiftUI/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  codeAlongWithMapKitforSwiftUI
+//
+//  Created by Laurent B on 16/08/2023.
+//
+
+import MapKit
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Map()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ codeAlongWithMapKitforSwiftUI/codeAlongWithMapKitforSwiftUI/codeAlongWithMapKitforSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  codeAlongWithMapKitforSwiftUIApp.swift
+//  codeAlongWithMapKitforSwiftUI
+//
+//  Created by Laurent B on 16/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct codeAlongWithMapKitforSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
