3cd8adada42bf8516364cae75011ab08a775157d
Laurent Brusa <wrmultitudes@gmail.com>
2023-08-16 14:19:55 +0200

    Update ContentView.swift

--- codeAlongWithMapKitforSwiftUI/codeAlongWithMapKitforSwiftUI/ContentView.swift
+++ codeAlongWithMapKitforSwiftUI/codeAlongWithMapKitforSwiftUI/ContentView.swift
@@ -8,9 +8,24 @@
 import MapKit
 import SwiftUI
 
+extension CLLocationCoordinate2D {
+    static var start = CLLocationCoordinate2D(latitude: 49.44625973148958, longitude: 1.0889092507665954)
+}
+
 struct ContentView: View {
     var body: some View {
-        Map()
+        Map {
+            Annotation("Start",
+                       coordinate: .start,
+                       anchor: .bottom
+            ){
+                Image(systemName: "flag")
+                    .padding(4)
+                    .foregroundStyle(.white)
+                    .background(Color.indigo)
+                    .cornerRadius(4)
+            }
+        }
     }
 }
 
